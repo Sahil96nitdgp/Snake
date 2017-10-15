d7aa3da4dabff9f22d8228881ee2f347431dd421
Sahil96nitdgp <sahil131196@gmail.com>
2017-10-16 00:43:13 +0530

    Game on C++

---
+++ MAIN.cpp
@@ -0,0 +1,176 @@
+   #include <iostream>
+   #include <cstdlib>
+   #include <conio.h>
+   #include <windows.h>
+   using namespace std;
+
+   const int width = 30;
+   const int height = 30;
+   int x,y,fruitx,fruity,score;
+   enum direction { STOP = 0, LEFT, RIGHT, UP, DOWN};
+   bool gameOver;
+   direction dir;
+   int tailx[100], taily[100];
+   int ntail;
+
+   void Setup()
+   {
+       gameOver= false;
+       dir = STOP;
+       x = width/2;
+       y=height/2;
+       fruitx= rand() % width-1;
+       fruity= rand() % height-1;
+       score=0;
+
+   }
+
+   void Draw()
+   {
+       bool print;
+       system("cls");
+       for(int i=0;i<width;i++)
+            cout << "-" ;
+
+
+       cout << endl;
+       for(int i=0;i<height;i++)
+       {
+
+           for(int j=0;j<width;j++)
+           {
+               if(i==y && j==x)
+                {
+                    cout << "O";
+                    continue;
+                }
+               if(j==0 || j==width-1)
+                    cout << "|";
+
+                if( i== fruity && j== fruitx)
+                    cout << "@";
+               else
+                 {
+                     print=false;
+                     for(int k=0; k<ntail;k++)
+                     {
+                         if(tailx[k]==j && taily[k]== i)
+                         {
+                             cout << "o";
+                             print=true;
+                         }
+                     }
+                     if(!print)
+                        cout << " ";
+                 }
+           }
+           cout << endl;
+       }
+
+       for(int i=0;i<width;i++)
+            cout << "-" ;
+        cout << "\n\nScore : " << score << endl;
+
+
+   }
+
+   void Input()
+   {
+
+        if(_kbhit())
+        {
+            switch(_getch())
+            {
+                case 'a':
+                    dir= LEFT;
+                break;
+                case 'w':
+                    dir= UP;
+                break;
+                case 'd':
+                    dir= RIGHT;
+                break;
+                case 's':
+                    dir= DOWN;
+                break;
+                case 'x':
+                    gameOver=true;
+                break;
+
+            }
+        }
+
+
+   }
+
+   void Logic()
+   {
+
+        int prevx = tailx[0];
+        int prevy = taily[0];
+        int prev2x, prev2y;
+        tailx[0]=x;
+        taily[0]=y;
+        for(int i=1; i< ntail; i++)
+        {
+            prev2x = tailx[i];
+            prev2y= taily[i];
+            tailx[i]=prevx;
+            taily[i]=prevy;
+            prevx=prev2x;
+            prevy=prev2y;
+
+        }
+        switch(dir)
+        {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        }
+
+        //if(x>width || x<0 || y<0 || y>height)
+          //  gameOver=true;
+
+          if(x>=width) x=0; else if(x<0) x=width-1;
+          if(y>=height) y=0; else if (y<0) y=height-1;
+
+        for(int i=0;i<ntail;i++)
+        {
+            if(tailx[i]==x&&taily[i]==y)
+                gameOver=true;
+        }
+
+
+        if(x==fruitx && y==fruity)
+        {
+
+            score+=10;
+            fruitx= rand() % width-1;
+            fruity= rand() % height-1;
+            ntail++;
+        }
+
+   }
+
+   int main()
+   {
+
+       Setup();
+       while(!gameOver)
+       {
+           Draw();
+           Input();
+           Logic();
+           Sleep(10);
+       }
+       return 0;
+   }
